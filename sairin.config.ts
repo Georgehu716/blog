dc8feee0f97cd9173919583c09899e8b6c33dc61
Georgehu716 <2992786775@qq.com>
2022-03-07 22:29:52 +0800

    update sairin config

--- sairin.config.ts
+++ sairin.config.ts
@@ -4,7 +4,7 @@ import BlogTheme from "@sairinjs/sairin-theme-minimal";
 
 export default {
   siteConfig: {
-    title: 'Sairin'
+    title: "Georgehu's blog"
   },
   theme: BlogTheme,
   themeConfig: {
@@ -12,11 +12,11 @@ export default {
     links: [
       {
         title: "GitHub",
-        url: "https://github.com/djyde/sairin",
+        url: "https://github.com/georgehu716",
       },
       {
         title: "Twitter",
-        url: "https://twitter.com/randyloop",
+        url: "https://twitter.com/Ezikiel716",
       },
     ],
   },
